9ec366a1674da1104862e56551b9b384450162f2
ANDANICQ <94098282+ANDANICQ@users.noreply.github.com>
2021-12-27 07:27:30 +0200

    Done

---
+++ FizzBuzz/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Iterator;
+
+public class Main 
+{
+
+	
+	//Receive value from main method and determine if 
+	//it is FzzBuzz, Fizz , Buzz or none of above
+	public static void print(int i)
+	{
+		if(i%3==0 && i%5==0)
+		{
+			System.out.println("FizzBuzz");
+		}else if(i%3 ==0)
+		{
+			System.out.println("Fizz");
+			
+		}else if(i%5==0)
+		{
+			System.out.println("Buzz");
+			
+		}else 
+		{
+			System.out.println(i);
+			
+		}
+		
+		
+	}
+	
+
+	public static void main(String[] args) 
+	{
+		//  prints the numbers from 1 to 100. But for multiples of 
+		//three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
+
+	    for (int i = 1; i <=100; i++) 
+	    {
+
+	    	System.out.print(i+" is ");
+	    	print(i);
+		}
+
+	}
+
+}
